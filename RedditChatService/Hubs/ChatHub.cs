4e60b4ab4f11090da93c47263356598a2115b22b
ben-hunter-hansen <ben.hunter.hansen@gmail.com>
2015-12-14 22:37:53 -0600

    added joingroup/leavegroup to chat hub

--- RedditChatService/Hubs/ChatHub.cs
+++ RedditChatService/Hubs/ChatHub.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
 
 namespace RedditChatService.Hubs
 {
 	public class ChatHub : Hub
 	{
+		public async Task JoinGroup(string groupName) {
+			await Groups.Add (Context.ConnectionId, groupName);
+			Clients.Group(groupName).GreetAll();
+		}
+
+		public async Task LeaveGroup(string groupName) {
+			await Groups.Remove (Context.ConnectionId, groupName);
+		}
+
 		public void Send(string name, string message)
 		{
 			Clients.All.addMessage(name, message);
